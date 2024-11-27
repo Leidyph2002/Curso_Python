2be143de18e7f7b47792392a38b24ac37075b9ee
Leidyph2002 <leidypasacaherrera@gmail.com>
2024-11-27 19:47:15 +0100

    Initial commit

---
+++ 10_loops.py
@@ -0,0 +1,103 @@
+# while
+
+# my_condition = 0
+
+# while True:
+#     print("ERROR DEL SISTEMA, SE NECESITA REINICIAR")
+#     my_condition +=2
+
+# my_condition = 0
+
+# while my_condition < 20:
+#     my_condition +=1
+#     if my_condition == 15:
+#         print("Se detiene la ejecución")
+#         break
+#     print(my_condition)
+
+# print("Fin de programa")
+
+
+# Tabla del 8
+
+# print("---------------------------------------------------------------------------")
+
+
+# my_condition = 0
+# numero = int (input("Dame un numero, no letras: "))
+
+# while my_condition < 10:
+#     my_condition +=1
+#     if my_condition == 11:
+#         print("Se detiene la ejecución")
+#         break
+#     resultado = my_condition * numero
+#     print(f"{my_condition} x {numero} = {resultado}")
+
+# print("Aqui tienes tu tabla de multiplicar")
+
+
+# print("---------------------------------------------------------------------------")
+
+# For
+
+# my_list = [12, 34, 45, 67, 80, 23, 6]
+
+# for item in my_list:
+#     print(item)
+
+# print("---------------------------------------------------------------------------")
+
+# my_tupla = (12, 34, 45, 67, 80, 23, 6)
+
+# for item in my_tupla:
+#     item = item * 8
+#     print(item)
+
+# print("---------------------------------------------------------------------------")
+
+# my_set = {12, 34, 45, 67, 80, 23, 6}
+
+# for item in my_set:
+#     item = item * 8
+#     print(item)
+
+# print("---------------------------------------------------------------------------")
+
+# my_dict = {"Nombre": "Pepe", 34: "Hola"}
+
+# for item in my_dict.values():
+#     print(my_dict[item])
+
+# print("---------------------------------------------------------------------------")
+
+# my_dict = {"Nombre": "Pepe", 34: "Hola"}
+
+# print(my_dict["Nombre"])
+
+print("---------------------------------------------------------------------------")
+
+my_dict = {"Nombre": "Pepe", 34: "Hola"}
+
+for item in my_dict:
+    if item == "Nombre":
+        print("Encontrada la clave Nombre")
+
+print("---------------------------------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 9_conditionals.py
@@ -0,0 +1,82 @@
+# Conditional
+
+# if
+
+# my_condition = True
+
+# if my_condition:
+#    print ("se ejecuta la condición if")
+
+
+# my_condition = 5 + 5
+
+# if my_condition == 10:
+#     print ("se ejecuta la condición if")
+#     var2 = 5 + 7
+#     print ("Imprimiendo la variable 2", var2)
+
+# var1 = 6
+
+# print (var2)
+
+
+#if, elif, else
+
+print("--------------------------------------------------------------------------------------------------------")
+
+print("Dame un numero, no letras")
+my_condition = int(input())
+
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 25:
+    print("Es igual a 25")
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20")
+
+print("Fin de programa")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
